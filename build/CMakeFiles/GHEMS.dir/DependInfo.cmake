0a9ab3b10ccacb23e9b9d4a434e39abc9e3d46b5
ColinWang <colin861209@gmail.com>
2021-03-08 20:32:41 +0800

    [ADD] .gitignore

--- build/CMakeFiles/GHEMS.dir/DependInfo.cmake
+++ build/CMakeFiles/GHEMS.dir/DependInfo.cmake
@@ -14,7 +14,6 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
   "/usr/include/mysql"
-  "/home/hems/glpk-4.65-built/src"
   )
 
 # Targets to which this target links.
